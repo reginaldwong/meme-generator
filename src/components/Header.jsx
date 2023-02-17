a63446d1ecd7f27f346822fbdc98485741a633df
Reginald Wong <reginald_wong@outlook.com>
2023-02-17 16:00:36 -0500

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <header>
+            <img src="./images/Troll Face.png" alt="troll face" className="header--image" />
+            <h2 className="header--title">MemeMachine9000</h2>
+            <h3 className="header--subtitle">Project 3</h3>
+        </header>
+    )
+}
+
+export default Header;
