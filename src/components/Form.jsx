bc358db1e1ae8d1135cd8e11663fde002b6859c7
Reginald Wong <reginald_wong@outlook.com>
2023-02-18 01:26:41 -0500

    add placeholder image and style Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,19 @@ import React from "react"
 
 const Form = () => {
     return (
-        <h1>Form goes here</h1>
+        <section>
+        <div className="form--container">
+            <div className="form--container--image">
+                <img src="/images/placeholder.png" alt="" />
+            </div>
+            <form action="" className="form--form">
+                <p>Enter the top text in the first bar and bottom text in the second bar!</p>
+                <input type="text" placeholder="Top Text" className="form--input"/>
+                <input type="text" placeholder="Bottom Text" className="form--input"/>
+                <button className="form--button">Get a new meme 🔥🔥</button>
+            </form>
+        </div>
+        </section>
     )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ const Header = () => {
     return (
         <header>
             <section className="header--navbar">
-            <img src="./images/Troll Face.png" alt="troll face" className="header--image" />
-            <h2 className="header--title">MemeMachine9000</h2>
-            <h3 className="header--subtitle">Project 3</h3>
+                <img src="./images/Troll Face.png" alt="troll face" className="header--image" />
+                <h2 className="header--title">MemeMachine9000</h2>
+                <h3 className="header--subtitle">Project 3</h3>
             </section>
         </header>
     )
