9ae0529c9afe12a5ff6b565b30fd8bff27c28018
Reginald Wong <reginald_wong@outlook.com>
2023-02-20 14:44:15 -0500

    add get random meme click event to button

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,27 @@
-import React from "react"
+import React, {useState} from "react"
+import memeData from "../memeData";
 
 const Form = () => {
+    const [memeImage, setMemeImage] = useState("");
+
+    const getMemeImage = () => {
+        const memesArray = memeData.data.memes;
+        const randomMeme = memesArray[Math.floor(Math.random()*memesArray.length)];
+        setMemeImage(randomMeme.url);
+    }
+
     return (
         <section>
         <div className="form--container">
             <div className="form--container--image">
-                <img src="/images/placeholder.png" alt="" />
+                <img src={memeImage} alt="" className="form--image" />
             </div>
-            <form action="" className="form--form">
+            <div action="" className="form--form">
                 <p>Enter the top text in the first bar and bottom text in the second bar!</p>
                 <input type="text" placeholder="Top Text" className="form--input"/>
                 <input type="text" placeholder="Bottom Text" className="form--input"/>
-                <button className="form--button">Get a new meme 🔥🔥</button>
-            </form>
+                <button className="form--button" onClick={getMemeImage}>Get a new meme 🔥🔥</button>
+            </div>
         </div>
         </section>
     )
