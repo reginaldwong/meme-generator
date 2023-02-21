2c76e5f75da33fd17b1983a0af8e777b7e915aaa
Reginald Wong <reginald_wong@outlook.com>
2023-02-21 15:54:26 -0500

    added meme name to get meme function

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,11 +3,13 @@ import memeData from "../memeData";
 
 const Form = () => {
     const [memeImage, setMemeImage] = useState("");
+    const [memeName, setMemeName] = useState("Enter the top text in the first bar and bottom text in the second bar");
 
     const getMemeImage = () => {
         const memesArray = memeData.data.memes;
         const randomMeme = memesArray[Math.floor(Math.random()*memesArray.length)];
         setMemeImage(randomMeme.url);
+        setMemeName(randomMeme.name)
     }
 
     return (
@@ -17,10 +19,11 @@ const Form = () => {
                 <img src={memeImage} alt="" className="form--image" />
             </div>
             <div action="" className="form--form">
+                <h3>{memeName}</h3>
                 <p>Enter the top text in the first bar and bottom text in the second bar!</p>
                 <input type="text" placeholder="Top Text" className="form--input"/>
                 <input type="text" placeholder="Bottom Text" className="form--input"/>
-                <button className="form--button" onClick={getMemeImage}>Get a new meme 🔥🔥</button>
+                <button className="form--button" onClick={getMemeImage} >Get a new meme 🔥🔥</button>
             </div>
         </div>
         </section>
