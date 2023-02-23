a04c739fc2bad92bd48e26808ad5115d30853823
Reginald Wong <reginald_wong@outlook.com>
2023-02-23 09:21:46 -0500

    fix padding issues with About component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import Header from './components/Header'
 import Form from './components/Form'
 import Footer from './components/Footer'
+import About from './components/About'
 
 function App() {
-  
+
   return (
     <div className="App">
       <Header />
       <Form />
+      <About />
       <Footer />
     </div>
   )
---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+const About = () => {
+    return (
+        <section>
+            <div className="about--container">
+            <h3>What is MemeMachine9000?</h3>
+            <p>MemeMachine9000 is an online image maker that lets users add captions to popular meme images.</p>
+            <h3>How to make a meme</h3>
+            <ol>
+                <li><strong>Choose a template.</strong> Press "Get a new meme" button to get a randomized meme of over 100 memes.</li>
+                <li><strong>Add text.</strong> When a desired meme has been chosen, enter the top text and bottom text desired to display on the meme.</li>
+                <li><strong>Save and share.</strong> Once you are happy with the meme you have created, you can share through social apps, or download the meme to your device.</li>
+            </ol>
+            </div>
+        </section>
+    )
+}
+
+export default About;
