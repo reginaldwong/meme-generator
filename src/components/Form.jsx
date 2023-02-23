5203ee0de92612eebf0012d8440adb68970d27d3
Reginald Wong <reginald_wong@outlook.com>
2023-02-23 00:01:52 -0500

    style footer

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -21,8 +21,8 @@ const Form = () => {
             <div action="" className="form--form">
                 <h3 className="form--name">{memeName}</h3>
                 <p>Enter the top text in the first bar and bottom text in the second bar!</p>
-                <input type="text" placeholder="Top Text" className="form--input"/>
-                <input type="text" placeholder="Bottom Text" className="form--input"/>
+                <input type="text" placeholder="Enter top text" className="form--input"/>
+                <input type="text" placeholder="Enter bottom text" className="form--input"/>
                 <button className="form--button" onClick={getMeme} >Get a new meme 🔥🔥</button>
             </div>
         </div>
