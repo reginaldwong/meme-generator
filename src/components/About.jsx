2fe9cad5899179ccabdb7bb39907b47056e830f2
Reginald Wong <reginald_wong@outlook.com>
2023-02-23 16:20:06 -0500

    add sticky footer

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,9 +4,9 @@ const About = () => {
     return (
         <section>
             <div className="about--container">
-            <h3>What is MemeMachine9000?</h3>
+            <h3 className="about--title">What is MemeMachine9000?</h3>
             <p>MemeMachine9000 is an online image maker that lets users add captions to popular meme images.</p>
-            <h3>How to make a meme</h3>
+            <h3 className="about--title">How to make a meme</h3>
             <ol>
                 <li><strong>Choose a template.</strong> Press "Get a new meme" button to get a randomized meme of over 100 memes.</li>
                 <li><strong>Add text.</strong> When a desired meme has been chosen, enter the top text and bottom text desired to display on the meme.</li>
