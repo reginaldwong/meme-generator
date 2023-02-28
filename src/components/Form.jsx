35ba56d9d905be77894f3e1ef41e999a144497ce
Reginald Wong <reginald_wong@outlook.com>
2023-02-27 23:26:36 -0500

    refactor state to make meme an object

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,11 @@ import React, {useState} from "react"
 import memeData from "../memeData";
 
 const Form = () => {
-    const [memeImage, setMemeImage] = useState("/images/placeholder.png");
+    const [memeImage, setMemeImage] = useState({
+        topText: "",
+        bottomText: "",
+        image:"/images/placeholder.png"
+    });
     const [memeName, setMemeName] = useState("");
 
     const getMeme = () => {
@@ -16,7 +20,7 @@ const Form = () => {
         <section>
         <div className="form--container">
             <div className="form--container--image">
-                <img src={memeImage} alt={memeName} className="form--image" />
+                <img src={memeImage.image} alt={memeName} className="form--image" />
             </div>
             <div action="" className="form--form">
                 <h3 className="form--name">{memeName}</h3>
